5704c598d809714c791db97d74346256b9e3666f
lenapaist <89094494+lenapaist@users.noreply.github.com>
2021-08-20 15:06:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,13 +6,33 @@ $(document).ready(function () {
     let modal = $(".modal");
     let modalCloseButton = $(".modal-close-button");
     let viewFlatsButton = $(".view-flats");
+    let flatPath = $(".flats path");
+    let flatLink = $(".flat-list li a");
     floorPath.on("mouseover", function () {
-        // убрала удаление класса активного этажа при наведении
-        //floorPath.removeClass(".current-floor");
         currentFloor = $(this).attr("data-floor");
         $(".counter").text(currentFloor);
     });
-    // добавила слушатель на событие клика по этажу и тогда удаляет предыдущий этаж и выделяет активный
+    flatLink.on("mouseover", function () {
+        flatPath.removeClass("current-flat");
+        flatLink.removeClass("current-link");
+        currentFlat = $(this).attr("data-link");
+        $(`[data-flat=${currentFlat}]`).addClass("current-flat");
+    });
+    flatPath.on("mouseover", function () {
+        flatLink.removeClass("current-link");
+        flatPath.removeClass("current-flat");
+        currentFlat = $(this).attr("data-flat");
+        $(`[data-link=${currentFlat}]`).addClass("current-link");
+    });
+    flatLink.on("mouseout", function () {
+        flatPath.removeClass("current-flat");
+        flatLink.removeClass("current-link");
+    });
+    flatPath.on("mouseout", function () {
+        flatPath.removeClass("current-flat");
+        flatLink.removeClass("current-link");
+    });
+    //
     floorPath.on("click", toggleModal);
 
     floorPath.on("click", function () {
